0daa6c37587d37da85c465ab073923035298fc7c
vit1412zavialov@gmail.com <vit1412zavialov@gmail.com>
2026-05-03 15:22:03 +0500

    add

---
+++ films/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Header from "./components/Header.vue"
+import {RouterView} from "vue-router"
+</script>
+
+<template>
+    <div>
+        <Header />
+        <RouterView />
+    </div>
+</template>
+
+<style scoped>
+
+</style>
